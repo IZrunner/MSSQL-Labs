e02a470f8269abc0844a081523d6d0836dbbdc84
IZrunner <masterchefchinatown@gmail.com>
2020-05-15 21:31:40 +0300

    problems occured
    failed to connect sqlserver to php

---
+++ lr-16/index.php
@@ -0,0 +1,17 @@
+<?php
+    phpinfo();
+    // try {
+    //     $srvName = "sql-server,1433";
+    //     $connOptions = [
+    //         "Database"=>"Phones_Repair_Service",
+    //         "Uid"=>"phpUser",
+    //         "PWD"=>"phpUserPWD"
+    //     ];
+    //     $conn = sqlsrv_connect($srvName, $connOptions);
+    //     if($conn == false)
+    //         die(FormatErrors(sqlsrv_errors()));
+    // } catch (Exception $e) {
+    //     echo("Unknown error occured!");
+    // }
+    // echo "Connected!";
+    ?>
